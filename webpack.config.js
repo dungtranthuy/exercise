0e2c42ad1792fa3df28463ade5e734789e4735c2
dungtranthuy <dung.tranthuy@pycogroup.com>
2019-04-29 13:38:45 +0700

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  // mode: process.env.NODE_ENV || 'development',
+  // resolve: {
+  //   modules: [path.resolve(__dirname, 'src'), 'node_modules']
+  // },
+  devServer: {
+    contentBase: './build'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader' // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        loaders: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html')
+    })
+  ]
+}
